a658e1c2a962c921f600b8e0f216061a876329c3
Yangming Guan <yangming6733@gmail.com>
2024-10-25 18:05:20 -0700

    second upload

--- background.js
+++ background.js
@@ -33,7 +33,6 @@ chrome.commands.onCommand.addListener((command) => {
       return parent;
     }
     console.log(focusedElement);
-    /html/body/div[1]/div/app/div/div/multi-wizard/div/ng-form/shipment-wizard/div/div[1]/section-recipient/section2/div/div/article/div/div/div/section-body/form-builder[3]/div/div[5]/div/div/div/div/div/textarea
     const address = focusedElement.querySelector(".slds-textarea.aws-textarea.ng-pristine.ng-valid.ng-not-empty.ng-valid-required.ng-touched")
     console.log("address", address);
     let nParents = getNParents(focusedElement,3);
